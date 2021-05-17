337ba46ebebbf143077ca2f65c20a7850b46e44f
r-evick <r.evick@hotmail.com>
2021-05-17 03:14:26 -0400

    Update script.js
    focus on name field

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('test');
+/*
+Treehouse Techdegree:
+FSJS Project 3 - Interactive Form
+Code by: Ryan Evick
+*/
+
+document.addEventListener('DOMContentLoaded', () => {  //allows JS to run no matter where it's placed in HTML
+    
+/*
+puts focus state on name field upon page load
+*/
+const nameInput = document.getElementById("name");
+nameInput.focus();
+
+
+
+
+
+
+});   
