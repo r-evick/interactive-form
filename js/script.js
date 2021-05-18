72f8489987a008ec226ed62ef98590e938976e5f
r-evick <r.evick@hotmail.com>
2021-05-18 01:26:26 -0400

    Update script.js
    hide 'other' text field unless selected

--- js/script.js
+++ js/script.js
@@ -12,6 +12,27 @@ puts focus state on name field upon page load
 const nameInput = document.getElementById("name");
 nameInput.focus();
 
+/*
+hide other job role field unless selected
+*/
+const jobRoleOther = document.getElementById("other-job-role");
+const jobRoleInput = document.getElementById("title");
+
+jobRoleInput.addEventListener('change', (e) => {
+  const selectedJob = e.target.value;
+  if (selectedJob === 'other') {
+      jobRoleOther.style.display = 'block';
+  } else {
+      jobRoleOther.style.display = 'none';
+  }
+});
+
+jobRoleOther.style.display = 'none';
+
+
+
+
+
 
 
 
