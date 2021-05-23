dde344351d0c9284c7cb417ca20790b8739e6ce4
r-evick <r.evick@hotmail.com>
2021-05-23 07:38:50 -0400

    Update script.js
    credit card set as default payment in payment info section,
    and payment method chosen is only one visible in UI

--- js/script.js
+++ js/script.js
@@ -10,15 +10,15 @@ document.addEventListener('DOMContentLoaded', () => {  //allows JS to run no mat
 /*
 puts focus state on name field upon page load
 */
-const nameInput = document.getElementById("name");
+const nameInput = document.getElementById('name');
 nameInput.focus();
 
 
 /*
 hide other job role field unless selected
 */
-const jobRoleOther = document.getElementById("other-job-role");
-const jobRoleInput = document.getElementById("title");
+const jobRoleOther = document.getElementById('other-job-role');
+const jobRoleInput = document.getElementById('title');
 
 jobRoleInput.addEventListener('change', (e) => {
   const selectedJob = e.target.value;
@@ -36,8 +36,8 @@ jobRoleOther.style.display = 'none';
 /*
 ensures color selection is associated with the proper theme
 */
-const shirtDesign = document.getElementById("design");
-const colorInput = document.getElementById("color");
+const shirtDesign = document.getElementById('design');
+const colorInput = document.getElementById('color');
 const colors = color.children;
 colorInput.disabled = true;  //found disable snipet at https://www.qodo.co.uk/blog/javascript-enabling-and-disabling-form-field-elements/ 
 
@@ -61,7 +61,7 @@ shirtDesign.addEventListener('change', (e) => {
 /*
 updates total cost to be sum of items selected
 */
-const activities = document.getElementById("activities");
+const activities = document.getElementById('activities');
 let cost = document.getElementById('activities-cost');
 let totalCost = 0; //starting cost is $0 until an activity is selected
 
@@ -79,10 +79,37 @@ activities.addEventListener('change', (e) => {
 });
 
 
+/*
+credit card set as default payment in payment info section,
+and payment method chosen is only one visible in UI
+*/
+const payment = document.getElementById('payment');
+const creditCard = document.getElementById('credit-card');
+const paypal = document.getElementById('paypal');
+const bitcoin = document.getElementById('bitcoin');
+paypal.hidden = true;
+bitcoin.hidden = true;
+payment.value = 'credit-card';
 
-
-
-
+payment.addEventListener('change', (e) => {
+    const paymentMethod = e.target.value; 
+    
+    if (paymentMethod === 'credit-card') {
+        paypal.hidden = true;
+        bitcoin.hidden = true;
+        creditCard.hidden = false;
+    }
+    if (paymentMethod === 'paypal') {
+        creditCard.hidden = true;
+        bitcoin.hidden = true;
+        paypal.hidden = false;
+    } 
+    if (paymentMethod === 'bitcoin') {
+        creditCard.hidden = true;
+        paypal.hidden = true;
+        bitcoin.hidden = false;
+    }
+});
 
 
 
